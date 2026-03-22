08e4dd2c2667b9b9e08b9f5a3b9485247b50347d
Fizxxc <fizzx909@gmail.com>
2026-03-23 00:24:20 +0700

    kographh cloud 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Ember Vault",
+  title: "KOGRAPH.INT CLOUD",
   description: "Local encrypted vault with KOGRAPH.INT"
 };
 
