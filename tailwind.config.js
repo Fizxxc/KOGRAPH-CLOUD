2c22b993d500c196b9055ba53f0d780cb90644e8
Fizxxc <fizzx909@gmail.com>
2026-03-23 00:16:50 +0700

    kographh cloud 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './lib/**/*.{js,ts,jsx,tsx,mdx}'
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      boxShadow: {
-        glow: '0 0 40px rgba(34,211,238,0.18)'
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
